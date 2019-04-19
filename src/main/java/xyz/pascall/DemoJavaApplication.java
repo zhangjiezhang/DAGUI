2be3fe02b5f79297ee52e7480bb5084b63eff82d
zhangjie <zhang_jie@travelsky.com>
2019-04-19 17:23:55 +0800

    add controller for hello url

--- src/main/java/xyz/pascall/DemoJavaApplication.java
+++ src/main/java/xyz/pascall/DemoJavaApplication.java
@@ -2,12 +2,19 @@ package xyz.pascall;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class DemoJavaApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoJavaApplication.class, args);
     }
 
+    @GetMapping("/hello")
+    public String hello(){
+        return "hello github";
+    }
 }
